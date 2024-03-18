9134c29582f22662991de353ac7e831627baa5b2
Levi Gomes <lgomes.lrg@gmail.com>
2024-03-18 20:03:19 -0300

    Criação dos arquivos do projeto

---
+++ LoginForm.Designer.cs
@@ -0,0 +1,117 @@
+﻿namespace FazendaSuinos
+{
+    partial class LoginForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label_titulo = new System.Windows.Forms.Label();
+            this.label_username = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label_senha = new System.Windows.Forms.Label();
+            this.botao_login = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label_titulo
+            // 
+            this.label_titulo.AutoSize = true;
+            this.label_titulo.Font = new System.Drawing.Font("Microsoft Sans Serif", 22.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label_titulo.Location = new System.Drawing.Point(317, 96);
+            this.label_titulo.Name = "label_titulo";
+            this.label_titulo.Size = new System.Drawing.Size(147, 42);
+            this.label_titulo.TabIndex = 0;
+            this.label_titulo.Text = "Rodolfo";
+            // 
+            // label_username
+            // 
+            this.label_username.AutoSize = true;
+            this.label_username.Location = new System.Drawing.Point(272, 164);
+            this.label_username.Name = "label_username";
+            this.label_username.Size = new System.Drawing.Size(70, 16);
+            this.label_username.TabIndex = 1;
+            this.label_username.Text = "Username";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(275, 183);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(230, 22);
+            this.textBox1.TabIndex = 2;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(275, 237);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(230, 22);
+            this.textBox2.TabIndex = 4;
+            // 
+            // label_senha
+            // 
+            this.label_senha.AutoSize = true;
+            this.label_senha.Location = new System.Drawing.Point(272, 218);
+            this.label_senha.Name = "label_senha";
+            this.label_senha.Size = new System.Drawing.Size(46, 16);
+            this.label_senha.TabIndex = 3;
+            this.label_senha.Text = "Senha";
+            // 
+            // botao_login
+            // 
+            this.botao_login.Location = new System.Drawing.Point(423, 282);
+            this.botao_login.Name = "botao_login";
+            this.botao_login.Size = new System.Drawing.Size(82, 28);
+            this.botao_login.TabIndex = 5;
+            this.botao_login.Text = "Login";
+            this.botao_login.UseVisualStyleBackColor = true;
+            // 
+            // LoginForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.botao_login);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.label_senha);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label_username);
+            this.Controls.Add(this.label_titulo);
+            this.Name = "LoginForm";
+            this.Text = "LoginForm";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label_titulo;
+        private System.Windows.Forms.Label label_username;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label_senha;
+        private System.Windows.Forms.Button botao_login;
+    }
+}
---
+++ MainForm.Designer.cs
@@ -0,0 +1,280 @@
+﻿namespace FazendaSuinos
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.menuStrip = new System.Windows.Forms.MenuStrip();
+            this.cadastrosToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.relatorioToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.pessoaToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.usuarioToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.técnicoToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.gerenteToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.empresaToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.produtorToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.fornecedorToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.propriedadeToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.pocilgaToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.loteToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.atividadesToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.visitaToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.acaoCorretivaToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.alimentacaoToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.vacinaToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.personalizadoToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.padroesToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.relatorio1ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.relatorio2ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.relatorio3ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.configuracoesToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuStrip.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // menuStrip
+            // 
+            this.menuStrip.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.menuStrip.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.cadastrosToolStripMenuItem,
+            this.relatorioToolStripMenuItem,
+            this.atividadesToolStripMenuItem,
+            this.configuracoesToolStripMenuItem});
+            this.menuStrip.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip.Name = "menuStrip";
+            this.menuStrip.Size = new System.Drawing.Size(1262, 28);
+            this.menuStrip.TabIndex = 0;
+            this.menuStrip.Text = "menuStrip";
+            // 
+            // cadastrosToolStripMenuItem
+            // 
+            this.cadastrosToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.pessoaToolStripMenuItem,
+            this.empresaToolStripMenuItem,
+            this.propriedadeToolStripMenuItem,
+            this.pocilgaToolStripMenuItem,
+            this.loteToolStripMenuItem});
+            this.cadastrosToolStripMenuItem.Name = "cadastrosToolStripMenuItem";
+            this.cadastrosToolStripMenuItem.Size = new System.Drawing.Size(88, 24);
+            this.cadastrosToolStripMenuItem.Text = "Cadastros";
+            // 
+            // relatorioToolStripMenuItem
+            // 
+            this.relatorioToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.padroesToolStripMenuItem,
+            this.personalizadoToolStripMenuItem});
+            this.relatorioToolStripMenuItem.Name = "relatorioToolStripMenuItem";
+            this.relatorioToolStripMenuItem.Size = new System.Drawing.Size(84, 24);
+            this.relatorioToolStripMenuItem.Text = "Relatório";
+            // 
+            // pessoaToolStripMenuItem
+            // 
+            this.pessoaToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.usuarioToolStripMenuItem,
+            this.técnicoToolStripMenuItem,
+            this.gerenteToolStripMenuItem});
+            this.pessoaToolStripMenuItem.Name = "pessoaToolStripMenuItem";
+            this.pessoaToolStripMenuItem.Size = new System.Drawing.Size(224, 26);
+            this.pessoaToolStripMenuItem.Text = "Pessoa";
+            // 
+            // usuarioToolStripMenuItem
+            // 
+            this.usuarioToolStripMenuItem.Name = "usuarioToolStripMenuItem";
+            this.usuarioToolStripMenuItem.Size = new System.Drawing.Size(224, 26);
+            this.usuarioToolStripMenuItem.Text = "Usuario";
+            // 
+            // técnicoToolStripMenuItem
+            // 
+            this.técnicoToolStripMenuItem.Name = "técnicoToolStripMenuItem";
+            this.técnicoToolStripMenuItem.Size = new System.Drawing.Size(224, 26);
+            this.técnicoToolStripMenuItem.Text = "Técnico";
+            // 
+            // gerenteToolStripMenuItem
+            // 
+            this.gerenteToolStripMenuItem.Name = "gerenteToolStripMenuItem";
+            this.gerenteToolStripMenuItem.Size = new System.Drawing.Size(224, 26);
+            this.gerenteToolStripMenuItem.Text = "Gerente";
+            // 
+            // empresaToolStripMenuItem
+            // 
+            this.empresaToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.produtorToolStripMenuItem,
+            this.fornecedorToolStripMenuItem});
+            this.empresaToolStripMenuItem.Name = "empresaToolStripMenuItem";
+            this.empresaToolStripMenuItem.Size = new System.Drawing.Size(224, 26);
+            this.empresaToolStripMenuItem.Text = "Empresa";
+            // 
+            // produtorToolStripMenuItem
+            // 
+            this.produtorToolStripMenuItem.Name = "produtorToolStripMenuItem";
+            this.produtorToolStripMenuItem.Size = new System.Drawing.Size(224, 26);
+            this.produtorToolStripMenuItem.Text = "Produtor";
+            // 
+            // fornecedorToolStripMenuItem
+            // 
+            this.fornecedorToolStripMenuItem.Name = "fornecedorToolStripMenuItem";
+            this.fornecedorToolStripMenuItem.Size = new System.Drawing.Size(224, 26);
+            this.fornecedorToolStripMenuItem.Text = "Fornecedor";
+            // 
+            // propriedadeToolStripMenuItem
+            // 
+            this.propriedadeToolStripMenuItem.Name = "propriedadeToolStripMenuItem";
+            this.propriedadeToolStripMenuItem.Size = new System.Drawing.Size(224, 26);
+            this.propriedadeToolStripMenuItem.Text = "Propriedade";
+            // 
+            // pocilgaToolStripMenuItem
+            // 
+            this.pocilgaToolStripMenuItem.Name = "pocilgaToolStripMenuItem";
+            this.pocilgaToolStripMenuItem.Size = new System.Drawing.Size(224, 26);
+            this.pocilgaToolStripMenuItem.Text = "Pocilga";
+            // 
+            // loteToolStripMenuItem
+            // 
+            this.loteToolStripMenuItem.Name = "loteToolStripMenuItem";
+            this.loteToolStripMenuItem.Size = new System.Drawing.Size(224, 26);
+            this.loteToolStripMenuItem.Text = "Lote";
+            // 
+            // atividadesToolStripMenuItem
+            // 
+            this.atividadesToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.acaoCorretivaToolStripMenuItem,
+            this.alimentacaoToolStripMenuItem,
+            this.vacinaToolStripMenuItem,
+            this.visitaToolStripMenuItem});
+            this.atividadesToolStripMenuItem.Name = "atividadesToolStripMenuItem";
+            this.atividadesToolStripMenuItem.Size = new System.Drawing.Size(93, 24);
+            this.atividadesToolStripMenuItem.Text = "Atividades";
+            // 
+            // visitaToolStripMenuItem
+            // 
+            this.visitaToolStripMenuItem.Name = "visitaToolStripMenuItem";
+            this.visitaToolStripMenuItem.Size = new System.Drawing.Size(224, 26);
+            this.visitaToolStripMenuItem.Text = "Visita";
+            // 
+            // açãoCorretivaToolStripMenuItem
+            // 
+            this.acaoCorretivaToolStripMenuItem.Name = "acaoCorretivaToolStripMenuItem";
+            this.acaoCorretivaToolStripMenuItem.Size = new System.Drawing.Size(224, 26);
+            this.acaoCorretivaToolStripMenuItem.Text = "Ação Corretiva";
+            // 
+            // alimentacaoToolStripMenuItem
+            // 
+            this.alimentacaoToolStripMenuItem.Name = "alimentacaoToolStripMenuItem";
+            this.alimentacaoToolStripMenuItem.Size = new System.Drawing.Size(224, 26);
+            this.alimentacaoToolStripMenuItem.Text = "Alimentação";
+            // 
+            // vacinaToolStripMenuItem
+            // 
+            this.vacinaToolStripMenuItem.Name = "vacinaToolStripMenuItem";
+            this.vacinaToolStripMenuItem.Size = new System.Drawing.Size(224, 26);
+            this.vacinaToolStripMenuItem.Text = "Vacina";
+            // 
+            // personalizadoToolStripMenuItem
+            // 
+            this.personalizadoToolStripMenuItem.Name = "personalizadoToolStripMenuItem";
+            this.personalizadoToolStripMenuItem.Size = new System.Drawing.Size(224, 26);
+            this.personalizadoToolStripMenuItem.Text = "Personalizado...";
+            // 
+            // padroesToolStripMenuItem
+            // 
+            this.padroesToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.relatorio1ToolStripMenuItem,
+            this.relatorio2ToolStripMenuItem,
+            this.relatorio3ToolStripMenuItem});
+            this.padroesToolStripMenuItem.Name = "padroesToolStripMenuItem";
+            this.padroesToolStripMenuItem.Size = new System.Drawing.Size(224, 26);
+            this.padroesToolStripMenuItem.Text = "Padrões";
+            // 
+            // relatorio1ToolStripMenuItem
+            // 
+            this.relatorio1ToolStripMenuItem.Name = "relatorio1ToolStripMenuItem";
+            this.relatorio1ToolStripMenuItem.Size = new System.Drawing.Size(224, 26);
+            this.relatorio1ToolStripMenuItem.Text = "Relatório 1";
+            // 
+            // relatorio2ToolStripMenuItem
+            // 
+            this.relatorio2ToolStripMenuItem.Name = "relatorio2ToolStripMenuItem";
+            this.relatorio2ToolStripMenuItem.Size = new System.Drawing.Size(224, 26);
+            this.relatorio2ToolStripMenuItem.Text = "Relatório 2";
+            // 
+            // relatorio3ToolStripMenuItem
+            // 
+            this.relatorio3ToolStripMenuItem.Name = "relatorio3ToolStripMenuItem";
+            this.relatorio3ToolStripMenuItem.Size = new System.Drawing.Size(224, 26);
+            this.relatorio3ToolStripMenuItem.Text = "Relatório 3";
+            // 
+            // configuracoesToolStripMenuItem
+            // 
+            this.configuracoesToolStripMenuItem.Name = "configuracoesToolStripMenuItem";
+            this.configuracoesToolStripMenuItem.Size = new System.Drawing.Size(118, 24);
+            this.configuracoesToolStripMenuItem.Text = "Configurações";
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1262, 673);
+            this.Controls.Add(this.menuStrip);
+            this.MainMenuStrip = this.menuStrip;
+            this.Name = "MainForm";
+            this.Text = "Form1";
+            this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
+            this.menuStrip.ResumeLayout(false);
+            this.menuStrip.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip;
+        private System.Windows.Forms.ToolStripMenuItem cadastrosToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem pessoaToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem usuarioToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem técnicoToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem gerenteToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem empresaToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem produtorToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem fornecedorToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem propriedadeToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem pocilgaToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem loteToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem relatorioToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem atividadesToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem padroesToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem relatorio1ToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem relatorio2ToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem relatorio3ToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem personalizadoToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem acaoCorretivaToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem alimentacaoToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem vacinaToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem visitaToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem configuracoesToolStripMenuItem;
+    }
+}
+
